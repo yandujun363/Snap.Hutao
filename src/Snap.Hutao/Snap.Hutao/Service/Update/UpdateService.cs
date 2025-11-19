113cf0528b3a136644c27383a0522dd5a4e4f81d
yangdujun363 <yangdujun363@outlook.com>
2025-11-19 21:29:31 +0800

    备份，移除更新检查

--- src/Snap.Hutao/Snap.Hutao/Service/Update/UpdateService.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Update/UpdateService.cs
@@ -30,123 +30,21 @@ internal sealed partial class UpdateService : IUpdateService
 
     public async ValueTask<CheckUpdateResult> CheckUpdateAsync(CancellationToken token = default)
     {
-        using (IServiceScope scope = serviceProvider.CreateScope())
+        // 直接返回"无需更新"，跳过所有网络检查
+        CheckUpdateResult checkUpdateResult = new()
         {
-            CheckUpdateResult checkUpdateResult = new();
-            try
-            {
-                ITaskContext taskContext = scope.ServiceProvider.GetRequiredService<ITaskContext>();
-                await taskContext.SwitchToBackgroundAsync();
+            Kind = CheckUpdateResultKind.AlreadyUpdated
+        };
 
-                HutaoInfrastructureClient infrastructureClient = scope.ServiceProvider.GetRequiredService<HutaoInfrastructureClient>();
-                HutaoResponse<HutaoPackageInformation> response = await infrastructureClient.GetHutaoVersionInformationAsync(token).ConfigureAwait(false);
-
-                if (!ResponseValidator.TryValidate(response, scope.ServiceProvider, out HutaoPackageInformation? packageInformation))
-                {
-                    checkUpdateResult.Kind = CheckUpdateResultKind.VersionApiInvalidResponse;
-                    return checkUpdateResult;
-                }
-
-                checkUpdateResult.Kind = CheckUpdateResultKind.UpdateAvailable;
-                checkUpdateResult.PackageInformation = packageInformation;
-
-                if (!LocalSetting.Get(SettingKeys.OverrideUpdateVersionComparison, false))
-                {
-                    // Launched in an updated version
-                    if (HutaoRuntime.Version >= checkUpdateResult.PackageInformation.Version)
-                    {
-                        checkUpdateResult.Kind = CheckUpdateResultKind.AlreadyUpdated;
-                        return checkUpdateResult;
-                    }
-                }
-
-                if (checkUpdateResult.PackageInformation.Validation is not { Length: > 0 })
-                {
-                    checkUpdateResult.Kind = CheckUpdateResultKind.VersionApiInvalidSha256;
-                }
-
-                return checkUpdateResult;
-            }
-            finally
-            {
-                UpdateInfo = checkUpdateResult.Kind switch
-                {
-                    CheckUpdateResultKind.UpdateAvailable => SH.FormatViewModelSettingUpdateAvailable(checkUpdateResult.PackageInformation?.Version.ToString()),
-                    CheckUpdateResultKind.AlreadyUpdated => SH.ViewModelSettingAlreadyUpdated,
-                    CheckUpdateResultKind.VersionApiInvalidResponse or CheckUpdateResultKind.VersionApiInvalidSha256 => SH.ViewModelSettingCheckUpdateFailed,
-                    _ => default,
-                };
-            }
-        }
+        UpdateInfo = SH.ViewModelSettingAlreadyUpdated;
+        return checkUpdateResult;
     }
 
     public async ValueTask TriggerUpdateAsync(CheckUpdateResult result, CancellationToken token = default)
     {
-        if (result.Kind is not CheckUpdateResultKind.UpdateAvailable)
-        {
-            return;
-        }
-
-        using (IServiceScope scope = serviceProvider.CreateScope())
-        {
-            ICurrentXamlWindowReference currentXamlWindowReference = scope.ServiceProvider.GetRequiredService<ICurrentXamlWindowReference>();
-            IContentDialogFactory contentDialogFactory = scope.ServiceProvider.GetRequiredService<IContentDialogFactory>();
-            IMessenger messenger = scope.ServiceProvider.GetRequiredService<IMessenger>();
-
-            if (currentXamlWindowReference.Window is null)
-            {
-                return;
-            }
-
-            try
-            {
-                ContentDialogResult installUpdateUserConsentResult = await contentDialogFactory
-                    .CreateForConfirmCancelAsync(
-                        SH.FormatViewTitleUpdatePackageAvailableTitle(result.PackageInformation?.Version),
-                        SH.ViewTitileUpdatePackageAvailableContent,
-                        ContentDialogButton.Primary)
-                    .ConfigureAwait(false);
-
-                if (installUpdateUserConsentResult is not ContentDialogResult.Primary)
-                {
-                    return;
-                }
-
-#if IS_ALPHA_BUILD
-                if (result.PackageInformation?.Mirrors.SingleOrDefault() is { MirrorType: Web.Hutao.HutaoPackageMirrorType.Browser } mirror)
-                {
-                    await Windows.System.Launcher.LaunchUriAsync(mirror.Url.ToUri());
-                }
-#else
-                await LaunchUpdaterAsync().ConfigureAwait(false);
-#endif
-            }
-            catch (Exception ex)
-            {
-                // Access to the path '?' is denied.
-                // 0x80070002 无法启动服务，原因可能是已被禁用或与其相关联的设备没有启动
-                // The process cannot access the file '?' because it is being used by another process.
-                // 0x80070005 Attempted to perform an unauthorized operation.
-                messenger.Send(InfoBarMessage.Error(ex));
-            }
-        }
+        // 更新功能已禁用，什么都不做
+        return;
     }
 
-    private async ValueTask LaunchUpdaterAsync()
-    {
-        string updaterTargetPath = HutaoRuntime.GetDataUpdateCacheDirectoryFile(UpdaterFilename);
-        InstalledLocation.CopyFileFromApplicationUri($"ms-appx:///{UpdaterFilename}", updaterTargetPath);
-
-        using (IServiceScope scope = serviceProvider.CreateScope())
-        {
-            HutaoUserOptions hutaoUserOptions = scope.ServiceProvider.GetRequiredService<HutaoUserOptions>();
-
-            string commandLine = new CommandLineBuilder()
-                .Append("update", await hutaoUserOptions.GetAccessTokenAsync().ConfigureAwait(false))
-                .ToString();
-
-            // The updater will request UAC permissions itself
-            ProcessFactory.StartUsingShellExecute(commandLine, updaterTargetPath);
-        }
-    }
+    // 移除 LaunchUpdaterAsync 方法，因为不再需要
 }
